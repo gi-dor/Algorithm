b92be67c331460956ee875e5163ff8210b425412
gi-dor <86302876+gi-dor@users.noreply.github.com>
2023-09-06 16:08:30 +0900

    Comment 아픈 동물 찾기.sql
    주석 추가 및 다른 답

--- 프로그래머스/lv1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
+++ 프로그래머스/lv1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
@@ -3,3 +3,14 @@ SELECT ANIMAl_ID , NAME
 from ANIMAL_INS
 where INTAKE_CONDITION = 'Sick'
 order by ANIMAL_ID
+
+-- where 절에 
+-- where INTAKE_CONDITION = Sick 라고 했다가 실패후
+-- where INTAKE_CONDITION Like %'Sick'% 실패...
+
+-- Like '%Sick%' 가 알맞은 문법
+
+SELECT ANIMAl_ID , NAME
+from ANIMAL_INS
+where INTAKE_CONDITION Like '%Sick%'
+order by ANIMAL_ID
