790d721845af138099572ebe34ba495186d05cf4
gi-dor <86302876+gi-dor@users.noreply.github.com>
2023-09-08 14:44:02 +0900

    Feat: 중복 제거하기 + 추가 답.sql
    기본 답과 추가로 쿼리를 이용한 답
    
    아직 쿼리의 개념을 제대로 파악하지 못한 상태

--- 프로그래머스/lv2/59408. 중복 제거하기/중복 제거하기.sql
+++ 프로그래머스/lv2/59408. 중복 제거하기/중복 제거하기.sql
@@ -3,3 +3,12 @@ select count(name)
 from ( select distinct name
       from animal_ins
       ) as A
+
+
+select count(distinct name) as A
+from animal_ins
+
+--
+-- 쿼리문의 사용이 익숙치 않고 제대로 개념을 파악하지 못해서 쉽지 않았음
+-- 중복제거인 distinct 로 해결하는 방법을 찾아보다가 해결하게됨
+-- 예를들어 총 행이 100개인데 A라는 이름을 가진 행이 5개라면 중복제거로 인해 96개가 됨 
