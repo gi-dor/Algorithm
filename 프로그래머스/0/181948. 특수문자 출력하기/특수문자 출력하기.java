84ec9edf2e16132c243ebe9314d43a57c02e2dd5
gi-dor <86302876+gi-dor@users.noreply.github.com>
2024-01-31 23:21:08 +0900

    Update 특수문자 출력하기.java

--- 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -4,5 +4,14 @@ public class Solution {
     public static void main(String[] args) {
         
         System.out.println("!@#$%^&*(\\'\"<>?:;");
+           // 특수 문자 입출력표
+        /*
+            입력 ->  출력
+            \\  ->  \
+            \'  -> ''
+            \"  -> "
+            따옴표 , 쌍따옴표 ,\  백틱 사용
+            
+        */
     }
 }
