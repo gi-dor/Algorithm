b7f2da2889db921d35425550e970352b630109ee
gi-dor <86302876+gi-dor@users.noreply.github.com>
2024-02-18 23:09:10 +0900

    [level 0] Title: n의 배수, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/181937. n의 배수/n의 배수.java
+++ 프로그래머스/0/181937. n의 배수/n의 배수.java
@@ -2,11 +2,8 @@ class Solution {
     public int solution(int num, int n) {
         int answer = 0;
        
-        if(num % n == 0){
-            return 1;
-        } else {
-            return 0;
-        }
+        answer = (num % n == 0 ) ? 1 : 0 ;
+        return answer;
       
     }
 }
